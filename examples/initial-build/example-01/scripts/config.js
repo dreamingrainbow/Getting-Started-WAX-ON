03afea672a9adcad81fc03a748bb0a2e9f11e49e
dreamingrainbow <michaeladennis@yahoo.com>
2025-12-22 12:15:38 -0600

    Create config.js

---
+++ examples/initial-build/example-01/scripts/config.js
@@ -0,0 +1,7 @@
+export const endpoints = {
+  testnet: "https://testnet.waxsweden.org",
+  mainnet: "https://wax.greymass.com"
+};
+
+export const privateKey = "YOUR_PRIVATE_KEY";
+export const contractAccount = "yourcontract1";
